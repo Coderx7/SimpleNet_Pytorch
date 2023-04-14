d7d2147b8f149fcb95a6962d1542b649e415ff87
seyyed hossein <coderx7@gmail.com>
2023-04-14 14:52:42 +0330

    update benchmark results

--- ImageNet/training_scripts/imagenet_training/results/add_acc_to_benchmarks.py
+++ ImageNet/training_scripts/imagenet_training/results/add_acc_to_benchmarks.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import argparse
 
@@ -11,7 +12,7 @@ parser.add_argument(
 )
 parser.add_argument(
     "--bench-csv",
-    default="./benchmark_inference_GTX1080_fp32_small_torch1.10.csv",
+    default="",
     type=str,
     metavar="FILENAME",
     help="the csv file for which you want to add accuracy",
@@ -23,10 +24,18 @@ def add_acc_to_csv(imagenet_results, csv_filename):
     df_imagenet_accs = df_imagenet_results[["model", "top1", "top5"]]
     df_csv = pd.read_csv(csv_filename)
     df_csv_acc = pd.merge(df_csv, df_imagenet_accs, on=["model"])
-    df_csv_acc.to_csv(csv_filename.replace(".csv", "_with_accuracy.csv"), index=False)
-    print(f"{csv_filename} is done")
+    df_csv_acc.to_csv(csv_filename.replace(".csv", "_acc.csv"), index=False)
+    print(f"--{csv_filename:<60} is processed.")
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    add_acc_to_csv(args.imagenet_results, args.bench_csv)
+    if args.bench_csv:
+        add_acc_to_csv(args.imagenet_results, args.bench_csv)
+    else:
+        print('Fetching all benchmark logs and adding accuracy to all in bulk...')
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        for file in os.listdir(current_dir):
+            if 'bench' in file and file.endswith('.csv'):
+                add_acc_to_csv(args.imagenet_results, file)
+        print(f'all done.')
