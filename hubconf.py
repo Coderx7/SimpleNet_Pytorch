dfce069f51114d2734fc96a26e2ca85fe2d6caaa
coderx7 <coderx7@gmail.com>
2023-05-02 19:04:14 +0330

    add experimental support for torch hub

---
+++ hubconf.py
@@ -0,0 +1,16 @@
+# Optional list of dependencies required by the package
+dependencies = ["torch"]
+
+from torchvision.models import get_model_weights, get_weight
+
+from ImageNet.simplenet import (
+    simplenet,
+    simplenetv1_5m_m1,
+    simplenetv1_5m_m2,
+    simplenetv1_9m_m1,
+    simplenetv1_9m_m2,
+    simplenetv1_small_m1_05,
+    simplenetv1_small_m2_05,
+    simplenetv1_small_m1_075,
+    simplenetv1_small_m2_075,
+)
